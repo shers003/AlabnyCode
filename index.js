09634fbb0d0becaf998fe73e2a30cea3d0ea687d
sonny sheridan <sonny.sheridan@yahoo.co.uk>
2020-11-01 20:43:50 +0000

    finished the mission 5

--- index.js
+++ index.js
@@ -56,17 +56,76 @@ const deleteItem = ()=>{
 	}
 };
 
-/********* Game at  pg 168 ***********/
+/********* Game ***********/
 var loops = 0;
+var playerScore = 0;
+var characterVisbible = false;
+var time = 2000
+
+//triggered when user clicks start
 const startGame = ()=>{
+	const difficulty = confirm('press ok for hard difficulty');
+	console.log(difficulty);
+	time = difficulty? 1500:2500;
 	gameLoop();
 };
+
+//Runs through the whole duration of game
 const gameLoop = ()=>{
+
+	const characters = document.querySelectorAll('.character');
+	const score = document.getElementById('score');
+
+	characterVisbible = !characterVisbible; //This will make it true as it is false, this will alternate each time function is run
+  characterCreator();
 	loops++;
-	console.log(loops);
 	if(loops<12){
-		setTimeout(gameLoop, 3000)
+
+		setTimeout(gameLoop, time)
 	}else{
-		alert('GAME OVER!')
+		alert('GAME OVER score: '+playerScore)
+		loops = 0;
+		playerScore = 0;
+		score.innerHTML = ''
 	}
-}
+};
+
+//used to create  characters
+//changes the characters classes depeing on if characterVisbible is true or false
+const characterCreator = ()=>{
+
+	const characters = document.querySelectorAll('.character');
+	const randomNum = Math.floor((Math.random() * 6) + 1);//Creates a random number floorm mthod rounds down
+	const classSet = characterVisbible? 'character visible':'character hidden';
+	const score = document.getElementById('score');
+	var clickcount = true;
+
+	characters.forEach((character)=>{
+		character.innerHTML = 'Guset';
+		character.className = classSet;
+		character.onclick = ()=>{
+			if(clickcount){
+				playerScore--;
+				console.log(score);
+				score.innerHTML = 'Score is '+playerScore;
+				clickcount = !clickcount
+			}else{
+			console.log('Abit much mate');
+		}
+	}
+	});
+	characters[randomNum-1].innerHTML = 'Tief'
+	characters[randomNum-1].className = classSet+' tief';
+	characters[randomNum-1].onclick = ()=>{
+		if(clickcount){
+			playerScore++;
+			console.log(score);
+			score.innerHTML = 'Score is '+playerScore;
+			clickcount = !clickcount
+		}else{
+			console.log('Abit much mate');
+		}
+
+	}
+
+};
